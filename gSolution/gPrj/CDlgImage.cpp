ffe4fd3504c060b13f0c770d3ca9cc8640aa9cc4
Daphne Kim <101921685+daphnekimyh@users.noreply.github.com>
2024-07-08 20:48:49 +0900

    Set Point Color

--- gSolution/gPrj/CDlgImage.cpp
+++ gSolution/gPrj/CDlgImage.cpp
@@ -94,7 +94,7 @@ void CDlgImage::drawData(CDC* pDC)
 {
 	CRect rect;
 	CPen pen;
-	pen.CreatePen(PS_SOLID, 5, RGB(0xff, 0, 0));
+	pen.CreatePen(PS_SOLID, 5, COLOR_RED);
 	CPen* pOldPen = pDC->SelectObject(&pen);
 	for (int i = 0; i < m_nDataCount; i++) {
 		rect.SetRect(m_ptData[i], m_ptData[i]);
